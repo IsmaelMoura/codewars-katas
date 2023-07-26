59dfbf867d63242e7072de98db16a91599ba6706
Ismael Moura <ismaeldesousa2014@outlook.com>
2023-07-26 15:37:43 -0300

    First commit

---
+++ src/test/kotlin/kyu_6/delete_occurrences_of_an_element_if_it_occurs_more_than_n_times/EnoughIsEnoughTest.kt
@@ -0,0 +1,31 @@
+package kyu_6.delete_occurrences_of_an_element_if_it_occurs_more_than_n_times
+
+import org.junit.Assert.assertArrayEquals
+import org.junit.Test
+
+class EnoughIsEnoughTest {
+    @Test
+    @Throws(Exception::class)
+    fun deleteNth() {
+        assertArrayEquals(
+            intArrayOf(20, 37, 21),
+            EnoughIsEnough.deleteNth(intArrayOf(20, 37, 20, 21), 1)
+        )
+        assertArrayEquals(
+            intArrayOf(1, 1, 3, 3, 7, 2, 2, 2),
+            EnoughIsEnough.deleteNth(intArrayOf(1, 1, 3, 3, 7, 2, 2, 2, 2), 3)
+        )
+        assertArrayEquals(
+            intArrayOf(1, 2, 3, 1, 1, 2, 2, 3, 3, 4, 5),
+            EnoughIsEnough.deleteNth(intArrayOf(1, 2, 3, 1, 1, 2, 1, 2, 3, 3, 2, 4, 5, 3, 1), 3)
+        )
+        assertArrayEquals(
+            intArrayOf(1, 1, 1, 1, 1),
+            EnoughIsEnough.deleteNth(intArrayOf(1, 1, 1, 1, 1), 5)
+        )
+        assertArrayEquals(
+            intArrayOf(),
+            EnoughIsEnough.deleteNth(intArrayOf(), 5)
+        )
+    }
+}
