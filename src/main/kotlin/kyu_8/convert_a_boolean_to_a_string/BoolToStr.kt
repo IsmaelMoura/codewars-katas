546a61361f3b1e91aa68c3821c0ee8521ed83144
Ismael Moura <ismaeldesousa2014@outlook.com>
2023-07-26 16:14:24 -0300

    Add kata link

--- src/main/kotlin/kyu_8/convert_a_boolean_to_a_string/BoolToStr.kt
+++ src/main/kotlin/kyu_8/convert_a_boolean_to_a_string/BoolToStr.kt
@@ -1,5 +1,8 @@
 package kyu_8.convert_a_boolean_to_a_string
 
+/**
+ * Kata link: https://www.codewars.com/kata/551b4501ac0447318f0009cd/train/kotlin
+ */
 object BoolToStr {
 
     fun convert(b: Boolean): String = b.toString()
